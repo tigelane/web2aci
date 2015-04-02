f387472502b53cf5b3422d9d02b0d36a8497eeb9
Tige Phillips <tige@tigelane.com>
2015-04-02 08:32:00 -0700

    test updates

--- cgi-bin/test_python.py
+++ cgi-bin/test_python.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+from datetime import datetime
 print 'Content-type: text/html\n'
 print '\n\n'
-print '<HTML><HEAD><TITLE>Python Test</TITLE></HEAD>'
-print '<BODY><H2>Hello there from a Python Script!</H2>'
+print '<HTML><HEAD><TITLE>Python Test File</TITLE></HEAD>'
+print '<BODY><H1>Python test script</H1>'
+print 'Python Version information:'
 print 'Version info:', sys.version_info
+print '<p>'
+print 'The current date and time are:'
+print datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print '<p>'
+
 print '</BODY></HTML>'
